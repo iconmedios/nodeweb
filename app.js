d8e3aacdb20aaa0a4761870f646934856c80c34e
iconmedios <jlrv75@gmail.com>
2022-07-09 22:35:20 -0500

    Sin modules

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+
+require('dotenv').config()
+const port = process.env.PORT;
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use (express.static('public'))
+app.use (expressLayouts)
+//app.use (express.static('public'));
+
+app.get('/', (req, res)=> {
+  res.render('inicio', {title: 'Inicio'} )
+})
+app.get('/acerca', (req, res)=> {
+  res.render('acerca', {
+    title: 'Acerca'
+  })
+})
+app.get('/contacto', (req, res)=> {
+  res.render('contacto', {
+    title: 'Contacto'
+  })
+})
+// layout personalizado
+app.get('/blog/post', function(req, res) {
+  res.render('blog/post.ejs', { layout: 'blog/blog-layout' });
+});
+
+app.get('*', (req, res)=> {
+    res.send('error 404')
+  })
+
+//   res.render('pages/index', {
+//     mascots: mascots,
+//     tagline: tagline
+// });
+
+app.listen(port, ()=>{
+    console.log(`Server en linea, puerto: ${port}`)
+});
